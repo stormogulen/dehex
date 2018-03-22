293c667c39c2c9d679d7bae5c17d99c080d8e054
Ulf Ejlertsson <ulf.ejlertsson@gmail.com>
2018-03-22 13:55:26 +0100

    Cleaned up the whole thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 //
 // main.rs
 //
+
+extern crate  pretty_hex;
+
+fn main() {
+
+    use pretty_hex::*;
+    let v = vec![222, 173, 190, 239, 202, 254, 32, 24];
+    assert_eq!(pretty_hex(&v), format!("{:?}", v.hex_dump()));
+
+    println!("{:?}", v.hex_dump());
+
+
+}
